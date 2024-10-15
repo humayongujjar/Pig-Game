ece3de28c6025134146639161bd4fd2369fcce6c
Humayon Ijaz <Humayongujjar8@gmail.com>
2024-10-15 11:55:51 +0500

    Update script.js

--- Pig Game/script.js
+++ Pig Game/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Selecting elements
+// Selecting element
 const player0El = document.querySelector('.player--0')
 const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0')
